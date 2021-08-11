a647189967bc78334063563dbb9563f3985f7b89
Pere Pages <pere.pages@ocado.com>
2021-08-11 17:24:55 +0200

    feat: express views with static content

--- src/Meadowlark/src/index.mjs
+++ src/Meadowlark/src/index.mjs
@@ -1,9 +1,14 @@
 import express from "express";
 import process from "process";
+import path from "path";
 import expressHandlebars from "express-handlebars";
 import { runExpress } from "../../shared/run-express.mjs";
+import { getDirname } from "../../shared/dirname.mjs";
+import { getRandomFortune } from "./about/fortunes.mjs";
 
 const PORT = process.env.PORT || 3000;
+const __dirname = getDirname(import.meta.url);
+const PUBLIC_FOLDER = path.join(__dirname, 'public');
 
 const makeResponseTypeText = (res) => res.type("text/plain");
 
@@ -16,16 +21,21 @@ app.engine(
   })
 );
 
+app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", "handlebars");
 
-app.get("/", (req, res) => res.render('home'));
+app.use(express.static(PUBLIC_FOLDER));
 
-app.get("/about", (req, res) => res.render('about'));
+app.get("/", (req, res) => res.render("home"));
+
+app.get("/about", (req, res) =>
+  res.render("about", { fortune: getRandomFortune() })
+);
 
 app.use((req, res) => {
   makeResponseTypeText(res);
   res.status(404);
-  res.render('404');
+  res.render("404");
 });
 
 // eslint-disable-next-line no-unused-vars
@@ -33,7 +43,7 @@ app.use((err, req, res, next) => {
   console.error(err.message);
   makeResponseTypeText(res);
   res.status(500);
-  res.render('500');
+  res.render("500");
 });
 
 runExpress(app, PORT);
